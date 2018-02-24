b472dd1380ed72d2384234efb1b2a9fe6fc9f0b9
Miguel Delacruz <delacruzsmiguel@Gmail.com>
2018-02-23 14:17:22 -1000

    change

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var fortunesList = []
+var fortuneList = ["You will get a walnut",
+"The time has come",
+"pokemons are real",
+"I will get extra credit for trying",
+"theres a snake in my boot",
+"reach for the sky"]
 document.getElementById('fortune-cookie-text')
+
+function generateFortruneCookie () {
+	if  (randomFortune == 0) {
+	return output = fortuneList[0];
+
+	else if  (randomFortune == 1) {
+	return output = fortuneList[1]
+}
+}
